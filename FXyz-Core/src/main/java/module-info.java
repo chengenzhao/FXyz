41f222fd52d8e37e44a0e883702bf31b89f97b5f
chengenzhao <chengen.zhao@mail.mcgill.ca>
2023-03-14 15:51:33 +0800

    copy source files

--- FXyz-Core/src/main/java/module-info.java
+++ FXyz-Core/src/main/java/module-info.java
@@ -1,20 +1,20 @@
 /**
  * F(X)yz
- *
+ * <p>
  * Copyright (c) 2013-2018, F(X)yz
  * All rights reserved.
- *
+ * <p>
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
+ * * Redistributions of source code must retain the above copyright
  * notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
+ * * Redistributions in binary form must reproduce the above copyright
  * notice, this list of conditions and the following disclaimer in the
  * documentation and/or other materials provided with the distribution.
- *     * Neither the name of F(X)yz, any associated website, nor the
+ * * Neither the name of F(X)yz, any associated website, nor the
  * names of its contributors may be used to endorse or promote products
  * derived from this software without specific prior written permission.
- *
+ * <p>
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -25,30 +25,31 @@
  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */ 
+ */
 
 module org.fxyz3d.core {
-    requires transitive javafx.controls;
-    requires static javafx.swing;
-    requires java.desktop;
-    requires java.logging;
-    requires static poly2tri.core;
-    requires java.compiler;
-    requires javafx.fxml;
+  requires transitive javafx.controls;
+  requires static javafx.swing;
+  requires java.desktop;
+  requires java.logging;
+  requires static poly2tri.core;
+  requires java.compiler;
+  requires javafx.fxml;
+  requires org.slf4j;
 
-    exports org.fxyz3d.geometry;
-    exports org.fxyz3d.io;
-    exports org.fxyz3d.scene;
-    exports org.fxyz3d.scene.paint;
-    exports org.fxyz3d.shapes;
-    exports org.fxyz3d.shapes.complex.cloth;
-    exports org.fxyz3d.shapes.composites;
-    exports org.fxyz3d.shapes.containers;
-    exports org.fxyz3d.shapes.polygon;
-    exports org.fxyz3d.shapes.polygon.symbolic;
-    exports org.fxyz3d.shapes.primitives;
-    exports org.fxyz3d.shapes.primitives.helper;
-    exports org.fxyz3d.tools;
-    exports org.fxyz3d.utils;
-    exports org.fxyz3d.utils.geom;
+  exports org.fxyz3d.geometry;
+  exports org.fxyz3d.io;
+  exports org.fxyz3d.scene;
+  exports org.fxyz3d.scene.paint;
+  exports org.fxyz3d.shapes;
+  exports org.fxyz3d.shapes.complex.cloth;
+  exports org.fxyz3d.shapes.composites;
+  exports org.fxyz3d.shapes.containers;
+  exports org.fxyz3d.shapes.polygon;
+  exports org.fxyz3d.shapes.polygon.symbolic;
+  exports org.fxyz3d.shapes.primitives;
+  exports org.fxyz3d.shapes.primitives.helper;
+  exports org.fxyz3d.tools;
+  exports org.fxyz3d.utils;
+  exports org.fxyz3d.utils.geom;
 }
